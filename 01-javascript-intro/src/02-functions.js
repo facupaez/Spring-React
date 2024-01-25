a7bb5bb208c0e34d573ddac577c156ed6c2fa4ae
facupaez <facu.pz10@gmail.com>
2024-01-24 21:52:58 -0300

    anonymus and arrow functions

--- 01-javascript-intro/src/02-functions.js
+++ 01-javascript-intro/src/02-functions.js
@@ -23,3 +23,23 @@ const result2 = sayHelloArgs("Clark");
 sayHello();
 console.log(result);
 console.log(result2);
+
+// ejemplo de funcion anonima
+const sayHelloAnonymus = function (name) {
+  const greeting = `Hola ${name} with anonymus function`;
+
+  return greeting;
+};
+
+const result3 = sayHelloAnonymus("Anonymus");
+console.log(result3);
+
+// ejemplo de funcion flecha y optimizamos funcion para ocupar 1 linea
+const sayHelloArrow = (name) => `Hola ${name} with arrow function`;
+const add = (a, b) => a + b;
+
+const result4 = sayHelloArrow("Anonymus");
+console.log(result4);
+
+const result5 = add(10, 5);
+console.log(`La suma es ${result5}`);
