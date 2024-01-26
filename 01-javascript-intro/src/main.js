53bb4b98f7d6b821f901b1a6f1294baf9bfd767d
facupaez <facu.pz10@gmail.com>
2024-01-25 21:08:32 -0300

    objects and methods relation

--- 01-javascript-intro/src/main.js
+++ 01-javascript-intro/src/main.js
@@ -1,17 +1,42 @@
 // creando objeto factura
-const invoice = {
+/* const invoice = {
   id: 10,
   type: "A",
   detail: "Compras de oficina",
   date: new Date(),
   client: "Asus",
   total: 1500,
-};
+};  */
 
 // mostrando informacion
-console.log(invoice);
-console.log(invoice.detail);
+/* console.log(invoice);
+console.log(invoice.detail); */
 
 // modificando informacion
-invoice.client = "Intel";
-console.log(invoice.client);
+/* invoice.client = "Intel";
+console.log(invoice.client); */
+
+// relacionando objetos
+const invoice = {
+  id: 10,
+  type: "A",
+  detail: "Compras de oficina",
+  date: new Date(),
+  client: {
+    id: 2,
+    name: "Intel",
+    lastName: "INC",
+    age: 20,
+  },
+  total: 1500,
+  greeting: function () {
+    return `Hola ${this.client.name}`;
+  },
+};
+
+invoice.client.name = "AMD";
+console.log(invoice);
+console.log(invoice.client.name);
+
+const greeting = invoice.greeting();
+console.log(greeting);
