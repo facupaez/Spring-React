b28e285e797c819fd7271dff62cc280797fa39ad
facupaez <facu.pz10@gmail.com>
2024-01-29 23:27:39 -0300

    building first component

---
+++ 02-react-intro/src/components/HelloWorld.jsx
@@ -0,0 +1,7 @@
+export function HelloWorld() {
+  return (
+    <>
+      <p>Hola Mundo!</p>
+    </>
+  );
+}
--- 02-react-intro/src/main.jsx
+++ 02-react-intro/src/main.jsx
@@ -1,24 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import { HelloWorld } from "../src/components/HelloWorld";
 
-// asi se crean los archivos jsx
-/* const h1 = React.createElement(
-  "div",
-  null,
-  React.createElement("ul", null, React.createElement("li", null, "Hola mundo!"))
-  ); */
-
-// esta forma utilizamos para crear elementos html
-const h1 = (
-  <div>
-    <ul>
-      <li>Hola mundo!</li>
-    </ul>
-  </div>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HelloWorld />
+  </React.StrictMode>
 );
-ReactDOM.createRoot(document.getElementById("root")).render(h1);
-/*   <React.StrictMode>
-    <App />
-  </React.StrictMode> */
