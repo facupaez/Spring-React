53315fe7b7f6e9e20e9926bdcd05fb07d54b5b80
facupaez <facu.pz10@gmail.com>
2024-01-29 23:15:29 -0300

    dom management

--- 02-react-intro/src/main.jsx
+++ 02-react-intro/src/main.jsx
@@ -3,8 +3,22 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// asi se crean los archivos jsx
+/* const h1 = React.createElement(
+  "div",
+  null,
+  React.createElement("ul", null, React.createElement("li", null, "Hola mundo!"))
+  ); */
+
+// esta forma utilizamos para crear elementos html
+const h1 = (
+  <div>
+    <ul>
+      <li>Hola mundo!</li>
+    </ul>
+  </div>
 );
+ReactDOM.createRoot(document.getElementById("root")).render(h1);
+/*   <React.StrictMode>
+    <App />
+  </React.StrictMode> */
