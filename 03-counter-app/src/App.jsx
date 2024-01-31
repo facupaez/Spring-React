aa5455753e94f616208c4c8dd60d48b620c319be
facupaez <facu.pz10@gmail.com>
2024-01-30 23:03:18 -0300

    handling useState hook

--- 03-counter-app/src/App.jsx
+++ 03-counter-app/src/App.jsx
@@ -1,23 +1,22 @@
-import React from "react";
+import { useState } from "react";
 
-const App = () => {
-  const counter = 0;
+const App = ({ value }) => {
+  // use state se divide en 3 partes
+  // primer parte: variable
+  // segunda parte: funcion a ejecutar
+  // tercera parte: valor inicial
+  const [counter, setCounter] = useState(value);
 
   const counterSum = () => {
-    console.log("click");
+    /* console.log("click"); */
+    setCounter(counter + 1);
   };
 
   return (
     <>
       <h1>Counter App</h1>
       <h2>El valor del contador es: {counter}</h2>
-      <button
-        onClick={() => {
-          counterSum();
-        }}
-      >
-        Incrementar contador +1
-      </button>
+      <button onClick={() => counterSum()}>Incrementar contador +1</button>
     </>
   );
 };
