c59f239a1ee71913a916d37dbfb714dbc1388945
facupaez <facu.pz10@gmail.com>
2024-02-01 22:05:55 -0300

    invoiceService function

--- 04-invoice-app/src/App.jsx
+++ 04-invoice-app/src/App.jsx
@@ -1,7 +1,14 @@
+import { getInvoice } from "./services/getInvoice";
+
 function App() {
+  const invoice = getInvoice();
   return (
     <>
       <h1>Ejemplo Factura</h1>
+      <ul>
+        <li>Id: {invoice.id}</li>
+        <li>Name: {invoice.name}</li>
+      </ul>
     </>
   );
 }
---
+++ 04-invoice-app/src/services/getInvoice.js
@@ -0,0 +1,6 @@
+import { invoice } from "../data/invoice";
+
+export const getInvoice = () => {
+  console.log(invoice);
+  return invoice;
+};
