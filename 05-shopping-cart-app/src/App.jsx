7a074f00364a59be916b98dceeffc9b8b5a39be4
facupaez <facu.pz10@gmail.com>
2024-02-04 22:12:46 -0300

    laying out shopping cart products

--- 05-shopping-cart-app/src/App.jsx
+++ 05-shopping-cart-app/src/App.jsx
@@ -65,6 +65,37 @@ function App() {
             </div>
           </div>
         </div>
+
+        <div className="my-4 w-50">
+          <h3 className="text-center">Carro de compras</h3>
+          <table className="table table-striped table-hover">
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Precio</th>
+                <th>Cantidad</th>
+                <th>Total</th>
+                <th>Eliminar</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>nombre</td>
+                <td>precio</td>
+                <td>cantidad</td>
+                <td>total</td>
+                <td>eliminar</td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr>
+                <td colSpan="5" className="fw-bold">
+                  Total $123
+                </td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
       </div>
     </>
   );
