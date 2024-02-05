de4341de655bb167d1c070561b2210a4a5e9861b
facupaez <facu.pz10@gmail.com>
2024-02-04 21:59:46 -0300

    laying out product catalog

--- 05-shopping-cart-app/src/App.jsx
+++ 05-shopping-cart-app/src/App.jsx
@@ -1,7 +1,71 @@
 function App() {
   return (
     <>
-      <h1>Shopping Cart App</h1>
+      <div className="container">
+        <h3 className="text-center mt-2">Shopping Cart App</h3>
+        <div className="row">
+          <div className="col-4 my-2">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Teclado RGB HyperX</h5>
+                <p className="card-text">Teclado mecánico retroiluminado</p>
+                <p className="card-text">$1000</p>
+                <button className="btn btn-success">Agregar a carrito</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-4 my-2">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Teclado RGB HyperX</h5>
+                <p className="card-text">Teclado mecánico retroiluminado</p>
+                <p className="card-text">$1000</p>
+                <button className="btn btn-success">Agregar a carrito</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-4 my-2">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Teclado RGB HyperX</h5>
+                <p className="card-text">Teclado mecánico retroiluminado</p>
+                <p className="card-text">$1000</p>
+                <button className="btn btn-success">Agregar a carrito</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-4 my-2">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Teclado RGB HyperX</h5>
+                <p className="card-text">Teclado mecánico retroiluminado</p>
+                <p className="card-text">$1000</p>
+                <button className="btn btn-success">Agregar a carrito</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-4 my-2">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Teclado RGB HyperX</h5>
+                <p className="card-text">Teclado mecánico retroiluminado</p>
+                <p className="card-text">$1000</p>
+                <button className="btn btn-success">Agregar a carrito</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-4 my-2">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Teclado RGB HyperX</h5>
+                <p className="card-text">Teclado mecánico retroiluminado</p>
+                <p className="card-text">$1000</p>
+                <button className="btn btn-success">Agregar a carrito</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
