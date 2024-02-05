b071173ecb6c5f8861c38cfdf1225d7eb449c3e6
facupaez <facu.pz10@gmail.com>
2024-02-04 22:21:44 -0300

    loading products data

---
+++ 05-shopping-cart-app/src/data/products.js
@@ -0,0 +1,38 @@
+export const products = [
+  {
+    id: 1,
+    name: "Teclado Hyperx",
+    description: "Mecánico retroiluminado RGB",
+    price: 200,
+  },
+  {
+    id: 2,
+    name: "Mouse SteelSeries",
+    description: "Laser 400dpi White",
+    price: 100,
+  },
+  {
+    id: 3,
+    name: "Headsets Corsair",
+    description: "Black Bluetooth Inalambric",
+    price: 300,
+  },
+  {
+    id: 4,
+    name: "Notebook HP",
+    description: "Black 1TB ROM 16GB RAM i9",
+    price: 1000,
+  },
+  {
+    id: 5,
+    name: "Mousepad HyperX",
+    description: "Black XXXL Smooth",
+    price: 50,
+  },
+  {
+    id: 6,
+    name: "Intel Core i9",
+    description: "11900K Rocket Lake 5.3Ghz",
+    price: 800,
+  },
+];
