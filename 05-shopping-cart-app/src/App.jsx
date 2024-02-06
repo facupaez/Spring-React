250aebde66c13d29c116f73da41e5329b5da3c03
facupaez <facu.pz10@gmail.com>
2024-02-05 21:26:13 -0300

    getProducts and useEffect hook

--- 05-shopping-cart-app/src/App.jsx
+++ 05-shopping-cart-app/src/App.jsx
@@ -1,69 +1,33 @@
+import { useEffect } from "react";
+import { getProducts } from "./services/productService";
+import { useState } from "react";
+
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts(getProducts());
+  }, []);
+
   return (
     <>
       <div className="container">
         <h3 className="text-center mt-2">Shopping Cart App</h3>
         <div className="row">
-          <div className="col-4 my-2">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Teclado RGB HyperX</h5>
-                <p className="card-text">Teclado mecánico retroiluminado</p>
-                <p className="card-text">$1000</p>
-                <button className="btn btn-success">Agregar a carrito</button>
+          {products.map(({ id, name, description, price }) => (
+            <div className="col-4 my-2" key={id}>
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">{name}</h5>
+                  <p className="card-text">{description}</p>
+                  <p className="card-text">
+                    ${new Intl.NumberFormat("es-AR").format(price)}
+                  </p>
+                  <button className="btn btn-success">Agregar a carrito</button>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-4 my-2">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Teclado RGB HyperX</h5>
-                <p className="card-text">Teclado mecánico retroiluminado</p>
-                <p className="card-text">$1000</p>
-                <button className="btn btn-success">Agregar a carrito</button>
-              </div>
-            </div>
-          </div>
-          <div className="col-4 my-2">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Teclado RGB HyperX</h5>
-                <p className="card-text">Teclado mecánico retroiluminado</p>
-                <p className="card-text">$1000</p>
-                <button className="btn btn-success">Agregar a carrito</button>
-              </div>
-            </div>
-          </div>
-          <div className="col-4 my-2">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Teclado RGB HyperX</h5>
-                <p className="card-text">Teclado mecánico retroiluminado</p>
-                <p className="card-text">$1000</p>
-                <button className="btn btn-success">Agregar a carrito</button>
-              </div>
-            </div>
-          </div>
-          <div className="col-4 my-2">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Teclado RGB HyperX</h5>
-                <p className="card-text">Teclado mecánico retroiluminado</p>
-                <p className="card-text">$1000</p>
-                <button className="btn btn-success">Agregar a carrito</button>
-              </div>
-            </div>
-          </div>
-          <div className="col-4 my-2">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Teclado RGB HyperX</h5>
-                <p className="card-text">Teclado mecánico retroiluminado</p>
-                <p className="card-text">$1000</p>
-                <button className="btn btn-success">Agregar a carrito</button>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
 
         <div className="my-4 w-50">
