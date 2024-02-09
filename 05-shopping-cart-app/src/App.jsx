98b574ad127d3d5e692d7ab7048e96847b7f93a6
facupaez <facu.pz10@gmail.com>
2024-02-08 21:06:37 -0300

    update product quantity

--- 05-shopping-cart-app/src/App.jsx
+++ 05-shopping-cart-app/src/App.jsx
@@ -8,14 +8,36 @@ function App() {
   const [cartItems, setCartItems] = useState(initialCartItems);
 
   const handlerAddProductCart = (product) => {
-    setCartItems([
-      ...cartItems,
-      {
-        product: product,
-        quantity: 1,
-        total: product.price * product.quantity,
-      },
-    ]);
+    const hasItem = cartItems.find((item) => item.product.id === product.id);
+
+    if (hasItem) {
+      // verificar si ya hay un producto agregado con filter
+      /*setCartItems([
+        ...cartItems.filter((item) => item.product.id !== product.id),
+        {
+          product: product,
+          quantity: hasItem.quantity + 1,
+        },
+      ]); */
+
+      // verificar si ya hay un producto agregado con map
+      setCartItems(
+        cartItems.map((item) => {
+          if (item.product.id === product.id) {
+            item.quantity = item.quantity + 1;
+          }
+          return item;
+        })
+      );
+    } else {
+      setCartItems([
+        ...cartItems,
+        {
+          product: product,
+          quantity: 1,
+        },
+      ]);
+    }
   };
   return (
     <>
