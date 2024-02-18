0a8f15247ff86cb998eba85d3a750cbf5cbeb457
facupaez <facu.pz10@gmail.com>
2024-02-18 10:41:23 -0300

    setting JPA persistence context

--- Spring/src/main/java/com/paez/backend/cartapp/BackendCartappApplication.java
+++ Spring/src/main/java/com/paez/backend/cartapp/BackendCartappApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.paez.backend.cartapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class BackendCartappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(BackendCartappApplication.class, args);
 	}
 
 }
--- Spring/src/test/java/com/paez/backend/cartapp/BackendCartappApplicationTests.java
+++ Spring/src/test/java/com/paez/backend/cartapp/BackendCartappApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.paez.backend.cartapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class BackendCartappApplicationTests {
 
 	@Test
 	void contextLoads() {
