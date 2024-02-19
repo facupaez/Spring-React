d896121dc08095d4fe2e9e5325ffdd5ec8c7a801
facupaez <facu.pz10@gmail.com>
2024-02-19 20:55:05 -0300

    building backend usersapp project

---
+++ Spring/backend-usersapp/src/test/java/com/paez/usersapp/BackendUsersappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paez.usersapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendUsersappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
