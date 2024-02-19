2f7e29afb31f2f39f648f8b03a8b8861c7585bc1
facupaez <facu.pz10@gmail.com>
2024-02-19 11:06:59 -0300

    consuming rest api from backend in react

--- React/05-shopping-cart-app/src/services/productService.js
+++ React/05-shopping-cart-app/src/services/productService.js
@@ -1,6 +1,7 @@
-import { products } from "../data/products";
+export const getProducts = async () => {
+  const response = await fetch("http://localhost:8080/products");
+  const products = await response.json();
 
-export const getProducts = () => {
   return products;
 };
 
