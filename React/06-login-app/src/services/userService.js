6cf7eb6f061d847898e17cd13cc41cce2d2bc6d3
facupaez <facu.pz10@gmail.com>
2024-02-20 10:44:46 -0300

    connecting saveUser backend method with frontend

--- React/06-login-app/src/services/userService.js
+++ React/06-login-app/src/services/userService.js
@@ -1,12 +1,24 @@
 import axios from "axios";
 
+const BASE_URL = "http://localhost:8080/users";
+
 export const findAllUsers = async () => {
   try {
-    const response = await axios.get("http://localhost:8080/users");
+    const response = await axios.get(BASE_URL);
     return response;
   } catch (error) {
     console.log(error);
   }
 
   return null;
+};
+
+export const saveUser = async ({ username, email, password }) => {
+  try {
+    return axios.post(BASE_URL, { username, email, password });
+  } catch (error) {
+    console.log(error);
+  }
+
+  return undefined;
 };
