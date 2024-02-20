93b39beb7a5fd96a6db249841391147a64a213d4
facupaez <facu.pz10@gmail.com>
2024-02-19 22:15:31 -0300

    building User controller class with findAll and findById methods

---
+++ Spring/backend-usersapp/src/main/java/com/paez/usersapp/controllers/UserController.java
@@ -0,0 +1,32 @@
+package com.paez.usersapp.controllers;
+
+import com.paez.usersapp.entities.User;
+import com.paez.usersapp.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> findAll(){
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id){
+        Optional<User> userOptional = userService.findById(id);
+        if (userOptional.isPresent()){
+            return ResponseEntity.ok(userOptional.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
