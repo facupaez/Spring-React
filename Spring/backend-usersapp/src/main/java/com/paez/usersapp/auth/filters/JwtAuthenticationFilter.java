3af4b2247095cd7da283262741f563b4e68317a5
facupaez <facu.pz10@gmail.com>
2024-03-17 22:19:33 -0300

    implementing unsuccessfulAuthentication method

--- Spring/backend-usersapp/src/main/java/com/paez/usersapp/auth/filters/JwtAuthenticationFilter.java
+++ Spring/backend-usersapp/src/main/java/com/paez/usersapp/auth/filters/JwtAuthenticationFilter.java
@@ -72,5 +72,14 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+
+        // cuando falla enviamos msj de error
+        Map<String, Object> body = new HashMap<>();
+        body.put("message", "Error en la autenticación, username o password incorrecto.");
+        body.put("error", failed.getMessage());
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setStatus(401);
+        response.setContentType("application/json");
     }
 }
