8b458a7c792e01fed7b4aa1f413461378b4b264d
facupaez <facu.pz10@gmail.com>
2024-03-23 18:03:51 -0300

    implementing loadUserByUsername method in UserDetailsService

---
+++ Spring/backend-usersapp/src/main/java/com/paez/usersapp/services/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.paez.usersapp.services;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        // si el usuario no se encuentra lanzamos excepción
+        if (!username.equals("admin")){
+            throw new UsernameNotFoundException(String.format("El usuario %s no existe en el sistema", username));
+        }
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return new User(username, "12345", true, true, true, true, authorities);
+    }
+}
