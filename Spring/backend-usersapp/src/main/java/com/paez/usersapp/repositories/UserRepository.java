f911db8b1d6485187d107fdd4a1c77bda87d6723
facupaez <facu.pz10@gmail.com>
2024-03-25 22:58:00 -0300

    Login with JPA from database

--- Spring/backend-usersapp/src/main/java/com/paez/usersapp/repositories/UserRepository.java
+++ Spring/backend-usersapp/src/main/java/com/paez/usersapp/repositories/UserRepository.java
@@ -1,7 +1,17 @@
 package com.paez.usersapp.repositories;
 
 import com.paez.usersapp.entities.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    // buscar por username con nomenclatura CrudRepository
+    Optional<User> findByUsername(String username);
+
+    // buscar por username con consulta Query con hibernate JPA
+    @Query("select u from User u where u.username=?1")
+    Optional<User> getUserByUsername(String username);
 }
--- Spring/backend-usersapp/src/main/java/com/paez/usersapp/services/UserDetailsServiceImpl.java
+++ Spring/backend-usersapp/src/main/java/com/paez/usersapp/services/UserDetailsServiceImpl.java
@@ -1,27 +1,47 @@
 package com.paez.usersapp.services;
 
+import com.paez.usersapp.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
+    @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        // si el usuario no se encuentra lanzamos excepción
-        if (!username.equals("admin")){
+        Optional<com.paez.usersapp.entities.User> oUser = userRepository.getUserByUsername(username);
+
+        // si el usuario no se encuentra lanzamos excepción (hardcode)
+
+/*        if (!username.equals("admin")){
+            throw new UsernameNotFoundException(String.format("El usuario %s no existe en el sistema", username));
+        }*/
+        // si el usuario no se encuentra lanzamos excepción (robusto)
+        if (!oUser.isPresent()){
             throw new UsernameNotFoundException(String.format("El usuario %s no existe en el sistema", username));
         }
 
+        com.paez.usersapp.entities.User user = oUser.orElseThrow();
+
         List<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
         authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
 
-        return new User(username, "$2a$10$DOMDxjYyfZ/e7RcBfUpzqeaCs8pLgcizuiQWXPkU35nOhZlFcE9MS", true, true, true, true, authorities);
+        // hardcode
+        //return new User(username, "$2a$10$DOMDxjYyfZ/e7RcBfUpzqeaCs8pLgcizuiQWXPkU35nOhZlFcE9MS", true, true, true, true, authorities);
+
+        return new User(user.getUsername(), user.getPassword(), true, true, true, true, authorities);
     }
 }
