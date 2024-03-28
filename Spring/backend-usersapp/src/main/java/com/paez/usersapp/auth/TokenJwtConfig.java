b22ddab56f262e1454d9f9131fd776d8932929ec
facupaez <facu.pz10@gmail.com>
2024-03-28 01:18:48 -0300

    creating JWT secret key

--- Spring/backend-usersapp/src/main/java/com/paez/usersapp/auth/TokenJwtConfig.java
+++ Spring/backend-usersapp/src/main/java/com/paez/usersapp/auth/TokenJwtConfig.java
@@ -1,8 +1,15 @@
 package com.paez.usersapp.auth;
 
+import io.jsonwebtoken.Jwts;
+import java.security.Key;
+
 public class TokenJwtConfig {
 
-    public final static String SECRET_KEY = "algun_token_con_frase_secreta";
+    // key hard code
+    // public final static String SECRET_KEY = "algun_token_con_frase_secreta";
+    
+    // llave secreta JWT
+    public final static Key SECRET_KEY = Jwts.SIG.HS256.key().build();
     public final static String PREFIX_TOKEN = "Bearer ";
     public final static String HEADER_AUTHORIZATION = "Authorization";
 
